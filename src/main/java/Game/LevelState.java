5da637d6bce1e27cfded9d6af4aa3d029cc0b700
Anthony Cerruti <tsunderebug@tsunderebug.com>
2018-04-19 19:44:29 -0700

    Package refactor

--- src/main/java/Game/LevelState.java
+++
@@ -1,8 +0,0 @@
-package Game;
-
-/**
- * Created by jamesmaron on 3/8/18.
- */
-public class LevelState {
-    ObjectManager manager;
-}
---
+++ src/main/java/com/github/zahir/level2/LevelState.java
@@ -0,0 +1,8 @@
+package com.github.zahir.level2;
+
+/**
+ * Created by jamesmaron on 3/8/18.
+ */
+public class LevelState {
+    ObjectManager manager;
+}
